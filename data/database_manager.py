775d98d0d5ba2340c2dae8fdd73f64401f3b04e4
RANYAKETFI <47351011+RANYAKETFI@users.noreply.github.com>
2022-02-15 10:00:38 +0100

    task

--- data/database_manager.py
+++ data/database_manager.py
@@ -4,29 +4,29 @@ from sqlite3 import Error
 
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
-    conn = None
+    conn=None
     try:
-        conn = None  # To-Do add a connection for the database
+      conn = sqlite3.connect(db_file)  # To-Do add a connection for the database
+      conn.ProgrammingError
     except Error as e:
         print(e)
-
+    finally:
     #To-Do return the connection 
-    return None 
+     return conn 
 
 def close_connection(conn):
     """ closes a connection to a database """
     conn.close()
 
 
-def select_all(conn):
-    """select all rows from our table using the conn we already created """
-    cur = conn.cursor()
-    query = "" # To-Do write the query to retrive all data from the longley table 
+def select_all(conn): 
 
+    cur = conn.cursor() 
+    query = "SELECT * FROM longley "
     cur.execute(query)
 
-    rows = None  # To-Do fetch all rows using the cursor cur
-
+      # To-Do fetch all rows using the cursor cur
+    rows = cur.fetchall() 
     return rows 
 
 
@@ -35,3 +35,4 @@ def print_rows(rows):
     # All of the function body is a todo task
     for row in rows:
         print(row)
+create_connection("longley.db")
